f2096cd42e84e0d9d0b8126936fd6ca95cefbc92
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-06-29 15:00:14 -0400

    working on redirecting to spotify login

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,32 +3,11 @@ import './App.css';
 import { Link } from "react-router-dom"
 
 function App() {
-  const CLIENT_ID="1f7ca06ee05740d7a725f74de05dbccd"
-  const CLIENT_SECRET="799cde9b441248a2bf3be283b44c41b2"
-  const REDIRECT_URI="http://localhost:3000"
-  const AUTH_ENDPOINT="https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE="token"
-
-  useEffect(() => {
-    function generateRandomString(length) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   
-    for (let i = 0; i < length; i++) {
-      text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-  }
-    console.log(generateRandomString)
-
-  }, [])
-
-
-
   return (
     <div className="App">
       <h1>Hello Spotify </h1>
-      <Link to={`${AUTH_ENDPOINT}?client_id-${CLIENT_ID}:${CLIENT_SECRET}&redirect_uri-${REDIRECT_URI}&response_type-${RESPONSE_TYPE}`}>Login to Spotify</Link>
+      <Link to="/callback">Login to Spotify</Link>
     </div>
   );
 }
