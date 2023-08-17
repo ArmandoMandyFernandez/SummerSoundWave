349d90435d3dd354af353486056571839725e811
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-08-16 21:29:00 -0400

    Auth done. Now need to add the api calls

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,40 +1,10 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
-import { useState, useEffect } from "react";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import HomePage from "./Pages/HomePage/HomePage";
 
 
-const getTokenFromUrl = () => {
-    return window.location.hash
-        .substring(1)
-        .split("&")
-        .reduce((initial, item) => {
-            let parts = item.split("=");
-            initial[parts[0]] = decodeURIComponent(parts[1]);
-            return initial;
-        }, {});
-};
-
 function App() {
-    const [spotifyToken, setSpotifyToken] = useState("");
-    const [loggedIn, setLoggedIn] = useState(false);
-
-    useEffect(() =>{
-        console.log(`This is what we derived from the URL:`, getTokenFromUrl())
-        const spotifyToken = getTokenFromUrl().access_token
-        window.location.hash="";
-        console.log(`This is our spotify token`, spotifyToken);
-
-        if (spotifyToken) {
-            setSpotifyToken(spotifyToken)
-            //use spotify api
-            setLoggedIn(true)
-        }
-    })
-
-
-    
     return (
         <div className="App">
             <Routes>
--- front-end/src/Pages/LoginPage/LoginPage.jsx
+++ front-end/src/Pages/LoginPage/LoginPage.jsx
@@ -1,11 +1,55 @@
 import './LoginPage.scss';
+import { useState, useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import axios from "axios";
+
+const spotifyApi = new SpotifyWebApi();
+
+const getTokenFromUrl = () => {
+    return window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial, item) => {
+            let parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+            return initial;
+        }, {});
+};
 
 
 function LoginPage(){
+    const [spotifyToken, setSpotifyToken] = useState("");
+    const [topTracks, setTopTracks] = useState({});
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [name, setName] = useState("")
+
+    useEffect(() =>{
+        console.log(`This is what we derived from the URL:`, getTokenFromUrl())
+        const spotifyToken = getTokenFromUrl().access_token
+        window.location.hash="";
+        console.log(`This is our spotify token`, spotifyToken);
+
+        if (spotifyToken) {
+            setSpotifyToken(spotifyToken)
+            spotifyApi.setAccessToken(spotifyToken)
+            spotifyApi.getMe().then((user)=>{
+                console.log(`using the getMe spotify call:`, user)
+                setName(name)
+                })
+            setLoggedIn(true)
+        }
+    })
+
+
     
     return(
         <section>
-            <a href="http://localhost:8888">Login to spotify</a>
+            {!loggedIn && <a href="http://localhost:8888">Login to spotify</a>}
+            {loggedIn && (
+                <>
+                <div>Welcome {name} </div>
+                </>
+            )}
         </section>
     )
 }
