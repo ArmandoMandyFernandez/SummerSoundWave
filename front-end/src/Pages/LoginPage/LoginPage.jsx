d02c223d79000f5189d75b2d65cac53d73e4f3ed
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-08-21 17:21:59 -0400

    added welcome message to summer rewind login page

--- front-end/src/Pages/LoginPage/LoginPage.jsx
+++ front-end/src/Pages/LoginPage/LoginPage.jsx
@@ -1,4 +1,4 @@
-import './LoginPage.scss';
+import "./LoginPage.scss";
 import { useState, useEffect } from "react";
 import SpotifyWebApi from "spotify-web-api-js";
 import axios from "axios";
@@ -16,42 +16,51 @@ const getTokenFromUrl = () => {
         }, {});
 };
 
-
-function LoginPage(){
+function LoginPage() {
     const [spotifyToken, setSpotifyToken] = useState("");
     const [topTracks, setTopTracks] = useState({});
     const [loggedIn, setLoggedIn] = useState(false);
-    const [name, setName] = useState("")
+    const [name, setName] = useState("");
 
-    useEffect(() =>{
-        console.log(`This is what we derived from the URL:`, getTokenFromUrl())
-        const spotifyToken = getTokenFromUrl().access_token
-        window.location.hash="";
+    useEffect(() => {
+        console.log(`This is what we derived from the URL:`, getTokenFromUrl());
+        const spotifyToken = getTokenFromUrl().access_token;
+        window.location.hash = "";
         console.log(`This is our spotify token`, spotifyToken);
 
         if (spotifyToken) {
-            setSpotifyToken(spotifyToken)
-            spotifyApi.setAccessToken(spotifyToken)
-            spotifyApi.getMe().then((user)=>{
-                console.log(`using the getMe spotify call:`, user)
-                setName(name)
-                })
-            setLoggedIn(true)
+            setSpotifyToken(spotifyToken);
+            spotifyApi.setAccessToken(spotifyToken);
+            spotifyApi.getMe().then((user) => {
+                console.log(`using the getMe spotify call:`, user);
+                setName(name);
+            });
+            setLoggedIn(true);
         }
-    })
+    });
 
 
-    
-    return(
+
+    return (
         <section>
             {!loggedIn && <a href="http://localhost:8888">Login to spotify</a>}
             {loggedIn && (
                 <>
-                <div>Welcome {name} </div>
+                    <h1>Welcome To Your Summer Rewind! </h1>
+                    <div>
+                        <h3>
+                            The Summer Rewind isn’t just another music playlist -
+                            it's a time capsule. It's the soundtrack to your
+                            summer adventures, the melodies that underscored
+                            your most cherished moments, and the rhythms that
+                            moved you. Dive in, and let the memories play.
+                        </h3>
+                        <button>Let's Get Started!</button>
+                    </div>
                 </>
             )}
         </section>
-    )
+    );
 }
 
 export default LoginPage;
