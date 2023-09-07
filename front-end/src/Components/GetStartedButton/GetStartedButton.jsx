d81303506095ed55ec51c1d5740b6503e6f93741
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-09-07 13:00:01 -0400

    added the start button

--- front-end/src/Components/GetStartedButton/GetStartedButton.jsx
+++ front-end/src/Components/GetStartedButton/GetStartedButton.jsx
@@ -1,7 +1,27 @@
 import './GetStartedButton.scss';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+
 
 
 function GetStartedButton(){
+
+
+
+    
+        useEffect(() => {
+                axios
+                    .get(`https://api.spotify.com/v1/me/top/tracks?limit=30`)
+                    .then((response) => {
+                        console.log(response.data);
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            }, []);
+
+
     return(
         <section>
             <button className='button__getTracks'>Get Started</button>
