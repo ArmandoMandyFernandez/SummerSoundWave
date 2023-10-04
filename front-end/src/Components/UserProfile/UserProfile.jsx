844d1d4d024affc5ef551840ffe80ac4307ac73e
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-10-04 16:35:53 -0400

    got rid of the error message for UserProfile dependency

--- front-end/src/Components/UserProfile/UserProfile.jsx
+++ front-end/src/Components/UserProfile/UserProfile.jsx
@@ -6,43 +6,42 @@ function UserProfile({ accessToken }) {
     const [user, setUser] = useState({});
     const [loading, setLoading] = useState(true);
 
-    async function fetchWebApi(endpoint, method, body) {
-        try {
-            const res = await axios({
-                method,
-                url: `https://api.spotify.com/${endpoint}`,
-                headers: {
-                    Authorization: `Bearer ${accessToken}`,
-                },
-                data: body,
-            });
-
-            if (res.status !== 200) {
-                throw new Error(`HTTP error! Status: ${res.status}`);
-            }
-
-            return res.data;
-        } catch (error) {
-            console.error("Error fetching data:", error);
-            throw error;
-        }
-    }
-
     useEffect(() => {
+        async function fetchWebApi(endpoint, method, body) {
+            try {
+                const res = await axios({
+                    method,
+                    url: `https://api.spotify.com/${endpoint}`,
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                    data: body,
+                });
+    
+                if (res.status !== 200) {
+                    throw new Error(`HTTP error! Status: ${res.status}`);
+                }
+    
+                return res.data;
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                throw error;
+            }
+        }
+    
         async function getMe() {
             try {
-                const userResponse = await fetchWebApi(
-                    'v1/me', 'GET'
-                );
+                const userResponse = await fetchWebApi('v1/me', 'GET');
                 setUser(userResponse);
                 setLoading(false);
             } catch (error) {
                 console.error("Error fetching user data:", error);
             }
         }
-
+    
         getMe();
     }, [accessToken]);
+    
 
     return (
         <div>
