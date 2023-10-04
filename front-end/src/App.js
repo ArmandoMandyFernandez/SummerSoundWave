5417bc027495936b82c6144d514c04f8dc482f8d
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-10-04 18:04:45 -0400

    added notes for making a playlist

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,22 @@
-import "./App.css";
+import React from "react";
+import { Route, Routes, Navigate } from "react-router-dom";
 import LoginPage from "./Pages/LoginPage/LoginPage";
 import HomePage from "./Pages/HomePage/HomePage";
 
 const code = new URLSearchParams(window.location.search).get("code");
 
 function App() {
-    return code ? <HomePage code={code}/> : <LoginPage />
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={
+                        code ? (<HomePage code={code} />) : (<Navigate to="/login" />)
+                    }
+                />
+                <Route path="/login" element={<LoginPage />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
