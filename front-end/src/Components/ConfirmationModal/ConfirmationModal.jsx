0c4e8b60381345e19861bfbc6a40b4306877ed83
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-10-13 18:13:29 -0400

    added the summer soundwave card

--- front-end/src/Components/ConfirmationModal/ConfirmationModal.jsx
+++ front-end/src/Components/ConfirmationModal/ConfirmationModal.jsx
@@ -1,4 +1,5 @@
 import "./ConfirmationModal.scss";
+import girl from "../../Assets/SummerSoundWave.png"
 
 function ConfirmationModal({ isOpen, onClose, top5Tracks }) {
     if (!isOpen) {
@@ -18,33 +19,42 @@ function ConfirmationModal({ isOpen, onClose, top5Tracks }) {
                     <h2 className="modal_header">
                         Playlist created successfully!
                     </h2>
-                    <p>Thanks for creating your playlist.</p>
+                    <p>Enjoy your new playlist!</p>
                     <p>
                         Share the top 5 songs of your SummerSoundWave with your
                         friends!
                     </p>
                 </div>
+
+                <div className="topFive_card">
+
+                <div className="topFive_image-container">
+                    <img src={girl} alt="soundwave icon of girl"  className="topFive_image"/>
+                </div>
                 
                 {top5Tracks && top5Tracks.length > 0 && (
-                    <div>
+                    <div className="topFive_outer-container">
+                        <div className="topFive_header-container">
+                            <h3>My Top 5 SummerSoundWave</h3>
+                        </div>
                         {top5Tracks.map((track, index) => (
-                            <div className="topTracks_container" key={track.id}>
-                                <div className="topTracks_container-number">
-                                    <h3 className="topTracks_list-number">
+                            <div className="topFive_container" key={track.id}>
+                                <div className="topFive_container-number">
+                                    <h3 className="topFive_list-number">
                                         {index + 1}.
                                     </h3>
                                 </div>
-                                <div className="topTracks_container-image">
+                                <div className="topFive_container-image">
                                     <img
                                         src={track.album.images[2].url}
                                         alt="Album Cover"
                                     />
                                 </div>
-                                <div className="topTracks_container-info">
-                                    <p className="topTracks_list-name">
+                                <div className="topFive_container-info">
+                                    <p className="topFive_list-name">
                                         {track.name}
                                     </p>
-                                    <p className="topTracks_list-artist">
+                                    <p className="topFive_list-artist">
                                         {track.artists
                                             .map((artist) => artist.name)
                                             .join(", ")}
@@ -54,6 +64,7 @@ function ConfirmationModal({ isOpen, onClose, top5Tracks }) {
                         ))}
                     </div>
                 )}
+                </div>
             </div>
         </div>
     );
