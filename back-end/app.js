c66700d774120c56b48e4dd3244eac5a1f4f4bdf
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-10-17 14:50:24 -0400

    fixed spelling in comment

--- back-end/app.js
+++ back-end/app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-// logiin post
+// login post
 app.post("/login", (req, res) => {
 
     const credentials = {
