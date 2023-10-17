5f54d79c61c0fb2fdbf6813428000c4e0d98d96e
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-10-17 16:58:09 -0400

    adding the path to the logo uptop

--- front-end/src/Components/Header/Header.jsx
+++ front-end/src/Components/Header/Header.jsx
@@ -1,11 +1,14 @@
 import logo from "../../Assets/the-6mix-logo-white.png";
 import "./Header.scss";
+import { Link } from "react-router-dom";
 
 function Header() {
 
     return (
         <section className="header">
+            <Link to="/">
             <img src={logo} alt="" className="header_image"/>
+            </Link>
         </section>
     );
 }
