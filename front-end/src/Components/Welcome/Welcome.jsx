4429a5de68e963bb56bb4c6ea4d3df5f6c519291
Mandy Fernandez <Mfernandezjr10@gmail.com>
2023-10-17 20:44:03 -0400

    something in welcome wasnt saved

--- front-end/src/Components/Welcome/Welcome.jsx
+++ front-end/src/Components/Welcome/Welcome.jsx
@@ -8,7 +8,7 @@ function Welcome( ) {
     return (
         <section className="welcome">
                 
-                <h1 className="welcome_header">Welcome to your 6Mix </h1>
+                <h1 className="welcome_header">Welcome to The 6Mix </h1>
                 <img src={SoundWave} alt="girl listening to music" className="welcome_image" />
                 <p className="welcome_description">Dive into a musical journey tailored just for you with The 6Mix! Unleash the magic of your last six months of listening, seamlessly crafted into a sick mix of your own using Spotify. It's your musical diary, bringing together a blend of cherished favorites and forgotten gems, echoing the vibrant tunes of your past half-year!</p>
         </section>
